694cb2be97892b2082e96c2dae8594e2b435e65d
DanielCubides <niroxdf@gmail.com>
2014-10-08 07:13:33 -0500

    Establecer Ruta de Navegacion

---
+++ TheLastHope/Assets/Standard Assets/Scripts/General Scripts/GoToScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToScreen : MonoBehaviour {
+	public string nombreNivel;
+	//private bool isSelected = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		Application.LoadLevel (nombreNivel);
+
+	}
+
+	void OnMouseEnter() {
+		transform.localScale = new Vector3 (transform.localScale.x + 0.01f,transform.localScale.y + 0.01f, transform.localScale.z);
+		Vector3 seleccion = new Vector3 ((transform.position.x + 0.01f), transform.position.y, 0);
+		transform.position = seleccion;
+	}
+
+	void OnMouseExit() {
+		transform.localScale = new Vector3 (transform.localScale.x - 0.01f,transform.localScale.y - 0.01f, transform.localScale.z);
+		Vector3 seleccion = new Vector3 ((transform.position.x - 0.01f), transform.position.y, 0);
+		transform.position = seleccion;
+	}
+}
